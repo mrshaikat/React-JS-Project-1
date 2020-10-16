309551ed6ab6e9c5b7f710f30f587f72174ff0ba
mrshaikat <mrshaikat0.cse@gmail.com>
2020-10-16 18:10:34 +0600

    Navbar and footer components done

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import logo from '../logo.svg';
+
+
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  <a classclassName="navbar-brand" href="#"><img src={logo} alt="logo" style={{ width:'35px' }}/></a>
+
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav m-auto">
+      <li className="nav-item active">
+        <a className="nav-link text-white text-uppercase ml-5" href="#">Home&nbsp;<i class="fas fa-home"></i><span class="sr-only">(current)</span></a>
+      </li>
+
+
+      <li className="nav-item">
+        <a className="nav-link text-white text-uppercase ml-5" href="#">News</a>
+      </li>
+
+      <li className="nav-item">
+        <a className="nav-link text-white text-uppercase ml-5" href="#">Contact Us</a>
+      </li>
+
+
+      
+    
+
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="E-mail" aria-label="Search"/>
+      <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">SIGN IN</button>
+    </form>
+  </div>
+</nav>
+    );
+}
+
+export default Navbar
